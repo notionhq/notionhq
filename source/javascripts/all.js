6b80e45296530b402c40e65c7473e49d90190669
Damian Rebman <drebman@elctech.com>
2012-07-11 10:03:01 -0700

    starting detail views

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
 //= require_tree .
+
+
+
+$(function() {
+
+  
+
+  $('.circle').click(function(e){
+      var name = $(this).data('name');
+      $('#person_detail .container').removeClass().addClass('container').addClass(name);
+      $('.circle').removeClass('active');
+      $(this).addClass('active');
+  }); 
+  
+
+  
+  
+});
