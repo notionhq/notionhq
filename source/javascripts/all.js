b0826daf862bb0f8fa82394c147337ae72dd11a6
James Pozdena <jpoz@simple.com>
2012-09-05 22:19:25 -0700

    stuff

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,8 +8,15 @@ $(function() {
 
   $('.circle').click(function(e){
       var name = $(this).data('name');
-      $('#person_detail .container').removeClass().addClass('container').addClass(name);
+      $('#person_detail')
+        .removeClass('closed')
+        .children('.container')
+          .removeClass()
+          .addClass('container')
+          .addClass(name);
+
       $('.circle').removeClass('active');
+
       $(this).addClass('active');
   }); 
   
